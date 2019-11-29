532aae34f782bbf1ede6d7b0e1d4a5a4f3edef9f
児島愛 <picoropit@gmail.com>
2019-11-29 22:56:54 +0900

    skill完成

--- js/app.js
+++ js/app.js
@@ -15,3 +15,24 @@ var swiper = new Swiper('.swiper-container', {
     prevEl: '.swiper-button-prev',
   },
 });
+
+
+
+// イラスト回転スライド
+let carousel = $(".carousel"),
+    currdeg = 0;
+
+$(".next").on("click", { d: "n" }, rotate);
+$(".prev").on("click", { d: "p" }, rotate);
+
+function rotate(e) {
+    if (e.data.d == "n") {
+        currdeg = currdeg - 60;
+    }
+    if (e.data.d == "p") {
+        currdeg = currdeg + 60;
+    }
+    carousel.css({
+        "transform": "rotateY(" + currdeg + "deg)"
+    });
+}
